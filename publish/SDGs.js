d1ad5bde3500b02bf75b3621a93bad2ece903f2a
Xinlei, Ormosia <114370154+OrmosiaCui@users.noreply.github.com>
2023-05-02 15:02:17 -0400

    Add files via upload

---
+++ publish/SDGs.js
@@ -0,0 +1,7 @@
+var predictBtn = document.getElementById("predict-btn");
+predictBtn.addEventListener("click", function() {
+  var resultImg = document.getElementById("result-img");
+  var resultLink = document.getElementById("result-link");
+  resultImg.style.display = "block";
+  resultLink.style.display = "block";
+});
